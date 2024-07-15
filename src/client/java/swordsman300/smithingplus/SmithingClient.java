be1c093a5bd1671e02026554f64bcdb0471b81a3
micahhanevich <micahjhanevich@gmail.com>
2024-07-15 15:03:48 -0400

    init commit

---
+++ src/client/java/swordsman300/smithingplus/SmithingClient.java
@@ -0,0 +1,10 @@
+package swordsman300.smithingplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SmithingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
